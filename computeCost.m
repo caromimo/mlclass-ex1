e9370ab466fae8f0306982a16eb559eb6a4534d1
Mike Williamson <blessedbyvirtuosity@gmail.com>
2011-10-23 10:42:14 -0400

    ComputeCost function is working.

--- computeCost.m
+++ computeCost.m
@@ -13,8 +13,12 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
+J = 1/(2*m) * sum(((X * theta) - y).^2);
 
-
+% Alternately:
+% predictions = X * theta;
+% sqrErrors = (predictions - y).^2;
+% J = 1/(2*m) * sum(sqrErrors);
 
 
 % =========================================================================
